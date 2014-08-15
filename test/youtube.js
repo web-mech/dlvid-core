f3ff6986ddfcc465bea44ce0ddff5640b182d07c
webmech <webmech@gmail.com>
2014-08-15 00:04:07 -0400

    updates for youtube tests, added test for download

--- test/youtube.js
+++ test/youtube.js
@@ -1,13 +1,27 @@
 var assert = require('better-assert'),
+  path = require('path'),
+  fs = require('fs'),
+  Stream = require('stream'),
+  streamAssert = require('stream-equal'),
   YouTubeExtractor = require('../lib/extractors/youtube');
 
 describe('youtube', function() {
+  var url = 'http://www.youtube.com/watch?v=GfAnyT9QitU';
   describe('getInfo', function() {
     it('Should get video results from a specified url', function(done) {
-      var yte = new YouTubeExtractor(),
-        url = 'http://www.youtube.com/watch?v=OwI1ZKCuOww';
-      yte.getInfo(url).done(function() {
+      var yte = new YouTubeExtractor();
+      yte.info(url).done(function(info) {
         assert(arguments.length === 1);
+        done();
+      });
+    });
+  });
+
+  describe('download', function() {
+    it('Should download a file', function(done) {
+      var yte = new YouTubeExtractor();
+      yte.download(url).done(function(file) {
+        assert(file instanceof Stream);
         done();
       });
     });
