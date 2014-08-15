240a3425f7effd3491fcf10c1ed6ea751e06201a
webmech <webmech@gmail.com>
2014-08-14 22:40:07 -0400

    added film leader test for youtube

--- test/youtube.js
+++ test/youtube.js
@@ -5,7 +5,7 @@ describe('youtube', function() {
   describe('getInfo', function() {
     it('Should get video results from a specified url', function(done) {
       var yte = new YouTubeExtractor(),
-        url = 'http://www.youtube.com/watch?v=TFE-gDl64og';
+        url = 'http://www.youtube.com/watch?v=OwI1ZKCuOww';
       yte.getInfo(url).done(function() {
         assert(arguments.length === 1);
         done();
