19426047d88d407ff3e60073538cff48c6ab38d4
webmech <webmech@gmail.com>
2014-08-14 22:36:54 -0400

    update vimeo tests

--- test/vimeo.js
+++ test/vimeo.js
@@ -1,42 +1,70 @@
 var assert = require('better-assert'),
+  path = require('path'),
+  fs = require('fs'),
+  streamAssert = require('stream-equal'),
   VimeoExtractor = require('../lib/extractors/vimeo'),
   config = require('../lib/config/config');
 
-
 describe('vimeo', function() {
-var url ='https://vimeo.com/channels/staffpicks/79426902';
-  describe('getId', function(){
-    it('Should return ids of vimeo clips',function(){
+  var url = 'https://vimeo.com/50872925';
+  describe('getId', function() {
+    it('Should return ids of vimeo clips', function() {
       var ve = new VimeoExtractor(config);
       var ids = ve.getId(url);
-      assert(ids === '79426902');
+      assert(ids === '50872925');
     });
   });
 
-  describe('getInfo', function(){
-    it('Should get meta-data information about a certain clip', function(done){
+  describe('getInfo', function() {
+    it('Should get meta-data information about a certain clip', function(done) {
       var ve = new VimeoExtractor(config);
-
-      ve.getInfo(url).done(function(result){
-        console.log(result);
+      ve.getInfo(url).done(function(result) {
         assert(result.request instanceof Object);
         assert(result.request.files instanceof Object);
         assert(typeof result.video.title === 'string');
-        //assert(result.video.title === 'Jay-Z - Fuckwithmeyouknowigotit (ft. Rick Ross)');
         done();
       });
     });
   });
-  /**
-  describe('download', function() {
-    it('Can download an mp4 for each clip', function(done) {
-      var ve = VimeoExtractor.new();
-      ve.download(urls).done(function(info){
-          assert(true);
-          done();
+
+  describe('selectFiles', function() {
+    it('Can select a file type with no options passed', function(done) {
+      var ve = new VimeoExtractor(config);
+      ve.getInfo(url).done(function(data) {
+        var files = data.request.files,
+          fileInfo = ve.selectFile(files, {});
+        assert(fileInfo.codec === 'h264');
+        assert(fileInfo.quality === 'sd');
+        done();
       });
-      
+    });
+
+    it('Will fall back to what is available when an invalid filter is passed', function(done) {
+      var ve = new VimeoExtractor(config);
+      ve.getInfo(url).done(function(data) {
+        var files = data.request.files,
+          fileInfo = ve.selectFile(files, {
+            type: 'vp8',
+            quality: 'hd'
+          });
+        assert(fileInfo.codec === 'h264');
+        assert(fileInfo.quality === 'sd');
+        done();
+      });
     });
   });
-**/
+
+  describe('download', function() {
+    it('Will download a file from vimeo', function(done) {
+      var video = path.resolve(__dirname, 'files/vimeo'),
+        ve = new VimeoExtractor(config),
+        vidStream = fs.createReadStream(video);
+      ve.download(url).done(function(file) {
+        streamAssert(vidStream, file, function(err, equals) {
+          assert(equals);
+          done();
+        });
+      });
+    });
+  });
 });
